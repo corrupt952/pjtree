7d95c11b5f2935180a3f14cc4f515fb9fdfeee8d
Kazuki Hasegawa <kazuki@Kazuki-no-MacBook-Air.local>
2013-07-09 14:37:36 +0900

    First commit.

---
+++ jwrite/main.py
@@ -0,0 +1,22 @@
+# -*- utf-8 -*-
+
+
+def main():
+    u""" Main method
+
+    main method.
+    """
+    import sys
+    import jwrite
+
+    argc = len(sys.argv)
+    if(argc < 4):
+        print('Usage: jwrite json encode path')
+        sys.exit(1)
+    data = jwrite.load_json(sys.argv[1], sys.argv[2])
+    jwrite.trace(data, sys.argv[3])
+    
+
+
+if __name__ == '__main__':
+    main()
